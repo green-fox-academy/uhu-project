2dac9b595538753b0a0e403d61ad7b99891d647e
Attila Vaczy <attila.vaczy@yahoo.com>
2016-02-16 16:59:38 +0100

    backend testcase

--- spec/backendTestSpec.js
+++ spec/backendTestSpec.js
@@ -1,7 +1,28 @@
 'use strict';
 
+var request = require('supertest'),
+express = require('express');
+var app = express();
+
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
+  });
+});
+
+app.get('/heartbeat', function(req, res){
+  res.status(200).send({});
+});
+
+describe('GET /heartbeat', function(){
+  it('should response with 200', function(done){
+    request(app)
+      .get('/heartbeat')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', 'json')
+      .expect(200, done)
+      .end(function(err, res) {
+        if (err) throw err;
+      });
   });
 });
