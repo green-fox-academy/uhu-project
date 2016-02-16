155b1092495e5284ab1881293f152da90e368faf
Bertalan Eva <eva0bertalan@gmail.com>
2016-02-16 13:18:19 +0100

    true spec tests

--- spec/backendTestSpec.js
+++ spec/backendTestSpec.js
@@ -5,9 +5,3 @@ describe("A suite", function() {
     expect(true).toBe(true);
   });
 });
-
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(false);
-  });
-});
--- spec/frontendTestSpec.js
+++ spec/frontendTestSpec.js
@@ -1,11 +1,7 @@
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
-});
+'use stict';
 
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
-    expect(true).toBe(false);
+    expect(true).toBe(true);
   });
 });
