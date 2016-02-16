6f34ec6a9d0260b16feae27b595524610f030fbc
Bertalan Eva <eva0bertalan@gmail.com>
2016-02-16 16:55:56 +0100

    add chrome-launcher refresh karma.conf separate karma from jasmine

--- main.js
+++ main.js
@@ -1,29 +1,10 @@
 'use strict';
 
-<<<<<<< HEAD
+require('newrelic');
 var server = require('./server.js');
 var app = server.myServer();
-=======
-require('newrelic');
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
->>>>>>> 4a786fddbd3deb833c821d7b9ae341f689d67e9b
 var port = require('./config.js').DEFAULT_PORT;
 
 app.listen(port, function () {
   console.log('Listening on port', port);
 });
-
-<<<<<<< HEAD
-=======
-function logRequest(req, res, next) {
-  var parts = [
-    new Date(),
-    req.method,
-    req.originalUrl,
-  ];
-  console.log(parts);
-  next();
-};
->>>>>>> 4a786fddbd3deb833c821d7b9ae341f689d67e9b
