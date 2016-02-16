938531b9e3e86304800082793308920c2df14169
André Zsolt <andrezsolt71@gmail.com>
2016-02-16 13:42:07 +0100

    main.js fixed

--- main.js
+++ main.js
@@ -4,19 +4,18 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var port = require('./config.js').DEFAULT_PORT;
-var heartbeat = require('./heartbeat.js');
+var heartBeat = require('./heartbeat.js');
 
 app.use(express.static('views'));
 app.use(bodyParser.json());
+app.use(logRequest);
 
 function helloWorld(req, res) {
   res.send('Hello World');
 }
 
 app.get('/', helloWorld);
-app.get('/heartbeat', function (req, res) {
-  res.send(res);
-});
+app.get('/heartbeat', heartBeat.heartBeat);
 
 app.listen(port, function () {
   console.log('Listening on port', port);
