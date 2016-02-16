45599dbe8549283f6dd1fa0fd95b2f6902fb2fec
André Zsolt <andrezsolt71@gmail.com>
2016-02-16 13:22:37 +0100

    main and heartbeat fixed

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 'use strict';
 
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
-var defaultPort = 3000;
+var port = require('./config.js').DEFAULT_PORT;
+var heartbeat = require('./heartbeat.js');
 
-//app.use(express.static('views'));
+app.use(express.static('views'));
+app.use(bodyParser.json());
 
 function helloWorld(req, res) {
   res.send('Hello World');
@@ -15,4 +18,16 @@ app.get('/heartbeat', function (req, res) {
   res.send(res);
 });
 
-app.listen(process.env.PORT || defaultPort);
+app.listen(port, function () {
+  console.log('Listening on port', port);
+});
+
+function logRequest(req, res, next) {
+  var parts = [
+    new Date(),
+    req.method,
+    req.originalUrl,
+  ];
+  console.log(parts);
+  next();
+};
