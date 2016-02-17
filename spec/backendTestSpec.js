24726c7a7e17528e61efa29ca5740c6e84bb11a5
Bertalan Eva <eva0bertalan@gmail.com>
2016-02-17 14:14:39 +0100

    add backendTestSpec with comments

---
+++ spec/backendTestSpec.js
@@ -0,0 +1,25 @@
+// 'use strict';
+
+// var request = require('supertest');
+// var server = require('../server.js');
+// var app = server.myServer();
+
+// describe("A suite", function() {
+//   it("contains spec with an expectation", function() {
+//     expect(true).toBe(true);
+//   });
+// });
+
+// describe('GET /heartbeat', function(){
+//   it('should response with 200', function(done){
+//     request(app)
+//       .get('/heartbeat')
+//       .set('Accept', 'application/json')
+//       .expect('Content-Type', /json/)
+//       .expect(200)
+//       .end(function(err, res) {
+//         if (err) throw err;
+//         done();
+//       });
+//   });
+// });
