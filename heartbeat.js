90c380ce19983be9380db157505827008df235ea
kristoftombacz <tombacz.kristof@gmail.com>
2016-02-18 16:31:09 +0100

    first steps of the frontend login

--- heartbeat.js
+++ heartbeat.js
@@ -3,22 +3,24 @@
 var pg = require('pg');
 var Logs = require('./logs.js');
 var url = require('./config.js').DATABASE_URL;
+var frontendLog = require('./logwaker.js');
 
 function heartBeat(db) {
   return function (req, res) {
     db('SELECT ok FROM heartbeat;', function(err, result) {
       if (err)
         {
-           var logger = new Logs();
-           logger.logCreator('heartBeatQueryConnect error', err, 'ERROR');
-           res.status(500).json({error: err});
+          var logger = new Logs();
+          logger.logCreator('heartBeatQueryConnect error', err, 'ERROR');
+          res.status(500).json({error: err});
         }
         else
-        {
-           res.status(200).json(result.rows);
-        }
-      });
-    };
+          {
+            res.status(200).json(result.rows);
+            frontendLog.newLogWaker('PAGE VIEW AT /heartbeat');
+          }
+    });
+  };
 }
 
 module.exports = {
