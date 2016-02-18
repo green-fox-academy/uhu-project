1b83ab54fa19259bc7f6fecb65bca0911aa2f8f2
André Zsolt <andrezsolt71@gmail.com>
2016-02-18 12:21:45 +0100

    first routing try oszt men

--- app/UHU.js
+++ app/UHU.js
@@ -1,7 +1,25 @@
 'use strict';
 
-var UHU = angular.module('UHU', []);
+var UHU = angular.module('UHU', ['ui.router']);
 var localURL = 'http://localhost:3000/';
 
 UHU.controller('MainController', function($scope) {
+});
+
+UHU.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  
+  $stateProvider
+    .state('all', {
+      url: '/all' ,
+      templateUrl: 'all.html'
+    })
+    .state('past', {
+      url: '/past' ,
+      templateUrl: 'past.html'
+    })
+    .state('ongoing', {
+      url: '/ongoing' ,
+      templateUrl: 'ongoing.html'
+    })
 });
