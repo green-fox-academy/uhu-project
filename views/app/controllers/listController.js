91100dbf9bbfee0164f6f96ee63c93b2c3dc42fd
André Zsolt <andrezsolt71@gmail.com>
2016-02-19 14:26:02 +0100

    image changing in calls and html modified

--- views/app/controllers/listController.js
+++ views/app/controllers/listController.js
@@ -1,13 +1,19 @@
 'use strict';
 
 var UHU = angular.module('UHU').controller('ListCtrl', function($scope) {
-  $scope.calls = call;
+  $scope.calls = calls;
+  $scope.statusChanger = function(call) {
+    var statusImageSrc = '/images/' + call.status + '.svg';
+    return statusImageSrc; 
+}
 });
 
 var now = moment().format('DD/MM/YYYY HH:MM');
 
-var call = [
+var calls = [
     {status: 'ongoing', startTime: now, elapsedTime:'03:04:11', endTime: now, id: 1},
     {status: 'ended', startTime: now, elapsedTime:'10:05:20', endTime: now, id: 2},
-    {status: 'ongoing', startTime: now, elapsedTime:'1:03:1', endTime: now, id: 3}
+    {status: 'ended'  , startTime: now, elapsedTime:'1:03:1', endTime: now, id: 3}
 ];
+
+
