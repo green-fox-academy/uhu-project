1b1c14ea16273897b5b3cdb65614ef2e53683e66
Bertalan Eva <eva0bertalan@gmail.com>
2016-02-24 15:43:08 +0100

    version 1 of elapsed timer

--- views/app/controllers/listController.js
+++ views/app/controllers/listController.js
@@ -1,9 +1,11 @@
 'use strict';
 var moment = require('moment');
 var UHU = require('../app');
+var numeral = require('numeral');
 
-UHU.controller('ListCtrl', function($scope) {
+UHU.controller('ListCtrl', function($scope, $interval) {
   $scope.calls = calls;
+  var count = 0;
   $scope.statusChanger = function(call) {
     var statusImageSrc = '/images/' + call.status + '.svg';
     return statusImageSrc;
@@ -11,30 +13,46 @@ UHU.controller('ListCtrl', function($scope) {
   $scope.myFilter = function(call, callStatus) {
     return call.status.match(/callStatus/) ? true : false;
   };
+  $interval(function() {
+    $scope.calls.forEach(function(call) {
+      if (call.status === 'ended') {
+        var elapsedTime = (end - start)/1000;
+        var formattedElapsedTime = numeral(elapsedTime).format('00:00:00');
+        call.elapsedTime = formattedElapsedTime;
+      } else if (call.status === 'incoming') {
+        call.elapsedTime = 'incoming';
+      } else {
+        count += 1;
+        call.elapsedTime = numeral(count).format('00:00:00');
+      }
+    });
+  }, 1000);
 });
 
-var start = moment().format('DD/MM/YYYY HH:MM');
-var end = moment().format('DD/MM/YYYY HH:MM');
+var start = moment().add(1, 'H');
+var end = moment().add(2, 'H');
 
 var calls = [
     {status: 'ongoing',
-     startTime: start,
-     elapsedTime:'03:04:11',
+     startTime: start, //.format('DD/MM/YYYY HH:MM'),
+     elapsedTime: start,
      endTime: '',
      id: 1},
 
     {status: 'ended',
-     startTime: start,
-     elapsedTime:'10:05:20',
-     endTime: end,
+     startTime: start, //.format('DD/MM/YYYY HH:MM'),
+     elapsedTime: start,
+     endTime: end, //.format('DD/MM/YYYY HH:MM'),
      id: 2},
 
     {status: 'ended',
-     startTime: start,
-     elapsedTime:'1:03:1',
-     endTime: end,
+     startTime: start, //.format('DD/MM/YYYY HH:MM'),
+     elapsedTime: start,
+     endTime: end,// .format('DD/MM/YYYY HH:MM'),
      id: 3},
-
+  
     {status: 'incoming',
+     startTime: start,
+     elapsedTime: 0,    
      id: 4}
 ];
