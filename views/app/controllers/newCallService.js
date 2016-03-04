c9d7ea341b5affe935179bc297068d277b19eed8
Bertalan Eva <eva0bertalan@gmail.com>
2016-03-04 14:24:07 +0100

    curl again

--- views/app/controllers/newCallService.js
+++ views/app/controllers/newCallService.js
@@ -31,7 +31,7 @@ UHU.service('newCallService', function(calls, $rootScope, $location) {
       return $location.protocol() + '://' + $location.host()
     }
 
-    var socket = io.connect(getBaseUrl() + ':4200');
+    var socket = io.connect('https://uhu-project.herokuapp.com/');
     socket.on('calls', function(data) {
       $rootScope.$apply(function () {
         _this.newCall(data);
