cbc283caba039736e661c97bcbf0c0dd7eefad38
Attila Vaczy <attila.vaczy@yahoo.com>
2016-03-07 16:43:44 +0100

    chart js inject error fixed

--- views/app/app.js
+++ views/app/app.js
@@ -2,5 +2,6 @@ var angular = require('angular');
 var angularMocks = require('angular-mocks');
 var angularUiRouter = require('angular-ui-router');
 var angularFilter = require('angular-filter');
+var angularChart = require('angular-chart.js');
 var UHU = angular.module('UHU', ['ui.router', 'angular.filter', 'chart.js']);
 module.exports = UHU;
