51f6206b91c212e79ab916d350242f96e594be92
Attila Vaczy <attila.vaczy@yahoo.com>
2016-03-08 18:36:39 +0100

    chart array transformer functions first version is added

--- views/app/controllers/chartController.js
+++ views/app/controllers/chartController.js
@@ -2,25 +2,25 @@ var UHU = require('../app');
 var angularChart = require('angular-chart.js');
 
 UHU.config( function (ChartJsProvider) {
-    // Configure all charts
     ChartJsProvider.setOptions({
       colours: ['#FF5252', '#FF8A80'],
       responsive: false
     });
-    // Configure all line charts
     ChartJsProvider.setOptions('Line', {
       datasetFill: false
     });
 });
-UHU.controller('chartCtrl', function($scope) {
 
-$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-$scope.series = ['Series A', 'Series B'];
-$scope.data = [
-  [65, 59, 80, 81, 56, 0, 40],
-  [28, 48, 0, 19, 86, 27, 90]
-];
-$scope.onClick = function (points, evt) {
-  console.log(points, evt);
-  };
+UHU.controller('chartCtrl', function($scope, newCallService) {
+$scope.calls = newCallService.getCalls();
+$scope.labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+$scope.series = ['call begin', 'call end', 'aborted calls', "unsuccesful calls" ];
+arrayTransformator();
+
+  function arrayTransformator () {
+    $scope.data = newCallService.getCalls().reduce(function(prev, call) {
+      prev[0].push(call.callbegin)
+      prev[1].push(call.callanswer);
+    return prev}, [[],[]]);
+  }
 });
