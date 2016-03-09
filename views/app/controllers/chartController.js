777df4caecb6e6964c1d5c997e406314914cf22f
Attila Vaczy <attila.vaczy@yahoo.com>
2016-03-09 16:34:36 +0100

    chart filled with mock data

--- views/app/controllers/chartController.js
+++ views/app/controllers/chartController.js
@@ -1,5 +1,6 @@
 var UHU = require('../app');
 var angularChart = require('angular-chart.js');
+var moment = require('moment');
 
 UHU.config( function (ChartJsProvider) {
     ChartJsProvider.setOptions({
@@ -11,16 +12,24 @@ UHU.config( function (ChartJsProvider) {
     });
 });
 
-UHU.controller('chartCtrl', function($scope, newCallService) {
-$scope.calls = newCallService.getCalls();
-$scope.labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-$scope.series = ['call begin', 'call end', 'aborted calls', "unsuccesful calls" ];
-arrayTransformator();
+UHU.controller('chartCtrl', function($scope) {
+//$scope.calls = newCallService.getCalls();
+$scope.labels = ["Seven hours before", "Six hours before", "Five hours before", "Four hours before", "Three hours before", "Two hours before", "One hours before"];
+$scope.series = ['Incoming Calls', 'Ended calls'];
+$scope.data = [
+  [65, 59, 80, 81, 56, 55, 40],
+  [28, 48, 40, 19, 86, 27, 90]
+];
+$scope.onClick = function (points, evt) {
+    console.log(points, evt);
+};
+/*arrayTransformator();
 
   function arrayTransformator () {
     $scope.data = newCallService.getCalls().reduce(function(prev, call) {
       prev[0].push(call.callbegin)
-      prev[1].push(call.callanswer);
+      prev[1].push(call.endTime);
     return prev}, [[],[]]);
-  }
+  }*/
+
 });
