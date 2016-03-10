760c4b3cfb7a4058e9766522202d5bd87cc31515
André Zsolt <andrezsolt71@gmail.com>
2016-03-10 16:29:01 +0100

    orderer function outsourced into listcalls deirestive

--- views/app/directives/listCallsDirective.js
+++ views/app/directives/listCallsDirective.js
@@ -3,7 +3,16 @@ var UHU = require('../app');
 UHU.directive('listcalls', function () {
   return {
     restrict: 'E',
-    scope: { calls: '=calls', callFilter: '=callFilter'},
-    templateUrl: '../../template/listCalls.html'
-  };
+    
+    templateUrl: '../../template/listCalls.html',
+    link: function(scope) {
+      scope.orderer = {value: 'blinky bill'};
+      scope.ordering = function(columnName) {
+        console.log(scope.orderer);
+        scope.orderer.value = columnName;
+        
+        console.log(scope);
+      }
+    }
+  };   
 });
