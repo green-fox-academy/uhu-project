03b4d220aa54058c034c9ed0c00a945a2a9693af
Attila Vaczy <attila.vaczy@yahoo.com>
2016-03-10 17:50:24 +0100

    chart array tranformator ver 1 function

--- views/app/controllers/chartController.js
+++ views/app/controllers/chartController.js
@@ -1,3 +1,4 @@
+'use strict';
 var UHU = require('../app');
 var angularChart = require('angular-chart.js');
 var moment = require('moment');
@@ -12,24 +13,27 @@ UHU.config( function (ChartJsProvider) {
     });
 });
 
-UHU.controller('chartCtrl', function($scope) {
-//$scope.calls = newCallService.getCalls();
-$scope.labels = ["Seven hours before", "Six hours before", "Five hours before", "Four hours before", "Three hours before", "Two hours before", "One hours before"];
-$scope.series = ['Incoming Calls', 'Ended calls'];
-$scope.data = [
-  [65, 59, 80, 81, 56, 55, 40],
-  [28, 48, 40, 19, 86, 27, 90]
-];
-$scope.onClick = function (points, evt) {
-    console.log(points, evt);
-};
-/*arrayTransformator();
+UHU.controller('chartCtrl', function($scope, newCallService) {
+  $scope.calls = newCallService.getCalls();
+  $scope.labels = ["Seven hours before", "Six hours before", "Five hours before", "Four hours before", "Three hours before", "Two hours before", "One hours before"];
+  $scope.series = ['Incoming Calls', 'Ended calls'];
+  arrayTransformator();
 
-  function arrayTransformator () {
-    $scope.data = newCallService.getCalls().reduce(function(prev, call) {
-      prev[0].push(call.callbegin)
-      prev[1].push(call.endTime);
-    return prev}, [[],[]]);
-  }*/
+  $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+  };
 
+function arrayTransformator() {
+  $scope.data = newCallService.getCalls().reduce(function(prev, call) {
+      var callBeginCounter = 0;
+      var callEndTimeCounter = 0;
+      if (moment().subtract(4, "hours").isSame(call.callbegin, "hours")) {
+          callBeginCounter ++;
+        } if (moment().subtract(4, "hours").isSame(call.endTime, "hours")) {
+          callEndTimeCounter ++;
+        }
+      prev[0].push(callBeginCounter);
+      prev[1].push(callEndTimeCounter);
+      return prev}, [[],[]]);
+  }
 });
