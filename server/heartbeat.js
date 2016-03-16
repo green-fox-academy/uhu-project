c81135e9551cfaf5b989b042c98255c73a833728
André Zsolt <andrezsolt71@gmail.com>
2016-03-16 15:39:57 +0100

    heartbeat fixed to newest jscs

--- server/heartbeat.js
+++ server/heartbeat.js
@@ -6,21 +6,16 @@ var url = require('./config.js').DATABASE_URL;
 
 function heartBeat(db) {
   return function (req, res) {
-    db('SELECT ok FROM heartbeat;', function(err, result) {
-      if (err)
-        {
-           var logger = new Logs();
-           logger.logError('heartBeatQueryConnect error', err, 'ERROR');
-           res.status(500).json({error: err});
-        }
-        else
-          {
-            res.status(200).json(result.rows);
-          }
+    db('SELECT ok FROM heartbeat;', function (err, result) {
+      if (err) {
+        var logger = new Logs();
+        logger.logError('heartBeatQueryConnect error', err, 'ERROR');
+        res.status(500).json({ error: err });
+      } else {
+        res.status(200).json(result.rows);
+      }
     });
   };
 }
 
-module.exports = {
-  heartBeat: heartBeat
-};
+module.exports = { heartBeat: heartBeat };
