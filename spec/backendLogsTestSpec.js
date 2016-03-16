26b48976efd068bcad49f261b2bfaea39dc8f4c0
Bertalan Eva <eva0bertalan@gmail.com>
2016-03-16 15:58:14 +0100

    fixed backendLogsTest with jscs

--- spec/backendLogsTestSpec.js
+++ spec/backendLogsTestSpec.js
@@ -2,35 +2,37 @@
 
 var Logs = require('../server/logs.js');
 
-describe('Logger', function() {
-  it("logMethodes shuld be defined", function() {
-  var logger = new Logs();
-  expect(logger.logDebug).toBeDefined();
-  expect(logger.logInfo).toBeDefined();
-  expect(logger.logWarn).toBeDefined();
-  expect(logger.logError).toBeDefined();
+describe('Logger', function () {
+  it('logMethodes shuld be defined', function () {
+    var logger = new Logs();
+    expect(logger.logDebug).toBeDefined();
+    expect(logger.logInfo).toBeDefined();
+    expect(logger.logWarn).toBeDefined();
+    expect(logger.logError).toBeDefined();
   });
 
-  it("should test console.log and date with default log level", function() {
+  it('should test console.log and date with default log level', function () {
     function fakeConsoleLog(log) {
       expect(log).toEqual('INFO date message status');
     }
 
     function FakeDate() {
-      FakeDate.prototype.toISOString = function() {
+      FakeDate.prototype.toISOString = function () {
         return 'date';
       };
     }
+
     var logger = new Logs(fakeConsoleLog, FakeDate);
     logger.logInfo('message', 'status');
   });
 
-  it("should test log level is ok", function() {
+  it('should test log level is ok', function () {
     function fakeConsoleLog(log) {
       expect(log).toEqual('WARN date message status');
     }
+
     function FakeDate() {
-      FakeDate.prototype.toISOString = function() {
+      FakeDate.prototype.toISOString = function () {
         return 'date';
       };
     }
@@ -41,20 +43,21 @@ describe('Logger', function() {
     logger.logWarn('message', 'status');
   });
 
-  it("should test log method is above log level", function() {
+  it('should test log method is above log level', function () {
     var FakeLoggingLevel = 'ERROR';
     var fakeConsole = {
-      log: function() {}
+      log: function () {},
     };
     spyOn(fakeConsole, 'log');
 
     var logger = new Logs(fakeConsole.log, FakeDate, FakeLoggingLevel);
     logger.logInfo('INFO date message status');
     function FakeDate() {
-      FakeDate.prototype.toISOString = function() {
+      FakeDate.prototype.toISOString = function () {
         return 'date';
       };
     }
+
     expect(fakeConsole.log).not.toHaveBeenCalled();
   });
 });
